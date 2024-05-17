5b3495bfbf388a205c21331ca010f9a554c041b1
WalidKarn <158018780+WalidKarn@users.noreply.github.com>
2024-05-18 02:41:24 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,423 @@
+tourpoint =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "จุฬาลงกรณ์มหาวิทยาลัย",
+        "website": "https://www.chula.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะอักษรศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.53152750000038,
+          13.737611578078543
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี (มจธ.)",
+        "website": "https://www.kmutt.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะวิศวกรรมศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.49441527116454,
+          13.651558574262381
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยธรรมศาสตร์ (ท่าพระจันทร์)",
+        "website": "https://tu.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะนิติศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.49064864550678,
+          13.757135526320099
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยเกษตรศาสตร์ (บางเขน)",
+        "website": "https://www.ku.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะวิทยาศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.56880215810918,
+          13.848139083838987
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ",
+        "website": "https://www.kmutnb.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะวิศวกรรมศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.51443477116294,
+          13.81919058171836
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง",
+        "website": "https://www.kmitl.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะวิศวกรรมศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.77908027116541,
+          13.72701215521532
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยศิลปกร (วังท่าพระ)",
+        "type": "รัฐบาล",
+        "website": "https://www.su.ac.th/",
+        "คณะที่เด่น": "คณะจิตรกรรมประติมากรรมและภาพพิมพ์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.49039399999918,
+          13.752843078758175
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยศรีนครินทรวิโรฒ (ประสานมิตร)",
+        "type": "รัฐบาล",
+        "website": "https://www.swu.ac.th/",
+        "คณะที่เด่น": "คณะศึกษาศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.56465765078815,
+          13.744880812829692
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยรามคำแหง",
+        "type": "รัฐบาล",
+        "website": "https://www.ru.ac.th/",
+        "คณะที่เด่น": "คณะนิติศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.61886590475257,
+          13.756251894549578
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยราชภัฏสวนสุนันทา",
+        "website": "https://ssru.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะครุศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.50857133862536,
+          13.775731257194892
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยราชภัฏธนบุรี",
+        "website": "https://dru1.dru.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะครุศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.49181472883521,
+          13.73405673376493
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "สถาบันบัณฑิตพัฒนบริหารศาสตร์ (นิด้า)",
+        "website": "https://nida.ac.th/en/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "เด่นบริหารศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          101.6661799658379,
+          14.854689278614543
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยเทคโนโลยีราชมงคลพระนคร",
+        "website": "https://www.rmutp.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะวิศวกรรมศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.51370099999849,
+          13.761249316528577
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยเทคโนโลยีราชมงคลกรุงเทพ",
+        "website": "https://www.rmutk.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะวิศวกรรมศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.52576949999792,
+          13.715727481324933
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยราชภัฏบ้านสมเด็จเจ้าพระยา",
+        "website": "https://www.bsru.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะครุศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.49030827116383,
+          13.732554999999238
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มหาวิทยาลัยสวนดุสิต",
+        "website": "https://www.dusit.ac.th/",
+        "type": "รัฐบาล",
+        "คณะที่เด่น": "คณะพยาบาลศาสตร์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.52610145766931,
+          13.767979000000324
+        ],
+        "type": "Point"
+      }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+          "name": "มหาวิทยาลัยเกริก",
+          "website": " https://www.krirk.ac.th/",
+          "type": "เอกชน",
+          "คณะที่เด่น": "คณะบริหารธุรกิจ"
+        },
+        "geometry": {
+          "coordinates": [
+            100.59995200000026,
+            13.873322415856805
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "มหาวิทยาลัยเทคโนโลยีมหานคร",
+          "website": "https://mut.ac.th/",
+          "type": "เอกชน",
+          "คณะที่เด่น": "คณะบริหารธุรกิจ"
+        },
+        "geometry": {
+          "coordinates": [
+            100.85618827116309,
+            13.844270499999638
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "มหาวิทยาลัยเกษมบัณฑิต",
+          "website": "http://www.kbu.ac.th/",
+          "type": "เอกชน",
+          "คณะที่เด่น": "คณะบริหารธุรกิจ"
+        },
+        "geometry": {
+          "coordinates": [
+            100.73655004495117,
+            13.80246824551007
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "มหาวิทยาลัยหอการค้าไทย",
+          "website": "https://www.utcc.ac.th/",
+          "type": "เอกชน",
+          "คณะที่เด่น": "คณะบริหารธุรกิจ"
+        },
+        "geometry": {
+          "coordinates": [
+            100.56023395767471,
+            13.778956079919638
+          ],
+          "type": "Point"
+        },
+        "id": 3
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "มหาวิทยาลัยสยาม",
+          "website": "https://www.siam.edu/",
+          "type": "เอกชน",
+          "คณะที่เด่น": "คณะวิศวกรรมศาสตร์"
+        },
+        "geometry": {
+          "coordinates": [
+            100.45335872883476,
+            13.720911654675021
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "มหาวิทยาลัยธุรกิจบัณฑิตย์",
+          "website": "https://www.dpu.ac.th/",
+          "type": "เอกชน",
+          "คณะที่เด่น": "คณะบริหารธุรกิจ"
+        },
+        "geometry": {
+          "coordinates": [
+            100.54994627116429,
+            13.870969331927071
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "มหาวิทยาลัยนานาชาติสแตมฟอร์ด",
+          "website": "https://www.stamford.edu/",
+          "type": "เอกชน",
+          "คณะที่เด่น": "คณะบริหารธุรกิจ"
+        },
+        "geometry": {
+          "coordinates": [
+            100.66144499073914,
+            13.735115284814384
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "มหาวิทยาลัยศรีปทุม",
+          "website": "https://www.spu.ac.th/",
+          "type": "เอกชน",
+          "คณะที่เด่น": "คณะเทคโนโลยีสารสนเทศ"
+        },
+        "geometry": {
+          "coordinates": [
+            100.58529535581664,
+            13.854466998724604
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "มหาวิทยาลัยเซนต์จอห์น",
+          "website": "https://www.sju.ac.th/",
+          "type": "เอกชน",
+          "คณะที่เด่น": "คณะบริหารธุรกิจ"
+        },
+        "geometry": {
+          "coordinates": [
+            100.56124399999868,
+            13.810928581348165
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "มหาวิทยาลัยเอเชียอาคเนย์",
+          "website": "https://sau.ac.th/sau-en/index.php",
+          "type": "เอกชน",
+          "คณะที่เด่น": "คณะบริหารธุรกิจ"
+        },
+        "geometry": {
+          "coordinates": [
+            100.35629981348916,
+            13.70696034656649
+          ],
+          "type": "Point"
+        }
+      }
+  ]
+}
